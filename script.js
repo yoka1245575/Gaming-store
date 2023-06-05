f19ca985396560db020d0c696a7531254fa705ef
yokamoka <130588314+yoka1245575@users.noreply.github.com>
2023-06-05 21:43:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var name;
+name = "efootball 2023";
+var price; 
+price = 1.3
+var gamename;
+gamename = "Fortnite";
+var gameprice; 
+gameprice = 0.0
+var gamename1;
+gamename1 = "Batman";
+var gameprice1; 
+gameprice1 = 4.23
+var gamename2;
+gamename2 = "PUBG battelgrounds";
+var gameprice2; 
+gameprice2 = 0.0
+var gamename3;
+gamename2 = "CALL OF DUTY VANGUARD";
+var gameprice3; 
+gameprice2 = 12.42
+var status;
+status = true;
+console.log(name);
+console.log(price);
+console.log(gamename);
+console.log(gameprice);
+console.log(gamename1);
+console.log(gameprice1);
+console.log(gamename2);
+console.log(gameprice2);
+console.log(status);
+
+var firstname ='yahia';
+var lastname ='mohamed';
+var age;
+age = 11;
+console.log (firstname+' '+lastname);
+console.log ('Your age is'+' '+age);
+if(age<11){
+    console.log ("you are young so, you can not play all games");
+   }else{
+       console.log ('you are old enough so,you can play all games')
+   }
